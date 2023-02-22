d716857f8dd755c3bc2fe45cb310f309f6905d7a
FS-Applied <76202706+FS-Applied@users.noreply.github.com>
2023-02-22 19:40:12 +0000

    Add project files.

---
+++ Delete Folders/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Status = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FolderName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(227, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Open Sans Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(73, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(247, Byte), Integer))
+        Me.Button4.Location = New System.Drawing.Point(329, 546)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 27)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(227, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Open Sans Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(73, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(247, Byte), Integer))
+        Me.Button3.Location = New System.Drawing.Point(187, 546)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(130, 27)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Delete Folders"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 10)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(69, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(360, 46)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = resources.GetString("Label2.Text")
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(227, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Open Sans Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(73, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(247, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(14, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 27)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Load List"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Status, Me.FolderName})
+        Me.DataGridView1.Location = New System.Drawing.Point(14, 112)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(418, 421)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'Status
+        '
+        Me.Status.HeaderText = "Status"
+        Me.Status.Name = "Status"
+        Me.Status.ReadOnly = True
+        Me.Status.Width = 80
+        '
+        'FolderName
+        '
+        Me.FolderName.HeaderText = "Folder Name"
+        Me.FolderName.Name = "FolderName"
+        Me.FolderName.ReadOnly = True
+        Me.FolderName.Width = 500
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 586)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Delete Folders"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Status As DataGridViewTextBoxColumn
+    Friend WithEvents FolderName As DataGridViewTextBoxColumn
+End Class
---
+++ Delete Folders/Form1.vb
@@ -0,0 +1,122 @@
+﻿
+Imports System.Text
+
+
+Public Class Form1
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        'Exit app
+        '
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Applied Systems", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Close()
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        'Check for items in the Data Grid
+        If DataGridView1.Rows.Count = 0 Then
+            MsgBox("Oops, did you forget something? I bet you did!!!", vbCritical, "Applied Systems")
+            Exit Sub
+        End If
+
+        'Proceed
+        '
+        Dim result As DialogResult = MessageBox.Show("All sub-folders and files inside those folders will be deleted. Are you sure you want to proceed?", "Applied Systems", MessageBoxButtons.YesNo)
+
+        If result = DialogResult.Yes Then
+
+            Dim strFolderFromList As String
+
+            For Each row As DataGridViewRow In DataGridView1.Rows
+
+                strFolderFromList = row.Cells(1).Value
+
+                Try
+                    'Does the magic
+                    '
+                    My.Computer.FileSystem.DeleteDirectory(strFolderFromList,
+                                                            Microsoft.VisualBasic.FileIO.UIOption.AllDialogs,
+                                                            Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin)
+
+                    'Add Status to the GridView
+                    '
+                    DataGridView1(0, row.Index).Value = "Deleted"
+                    DataGridView1.Refresh()
+
+
+                Catch ex As Exception
+
+                    If ex.Message.Contains("Could not find directory") Then
+                        'Folder not found
+                        DataGridView1(0, row.Index).Value = "Not found"
+                        DataGridView1.Refresh()
+                    Else
+                        'Other
+                        DataGridView1(0, row.Index).Value = ex.Message
+                        DataGridView1.Refresh()
+                    End If
+
+                End Try
+
+            Next
+
+
+        Else
+            'Does nothing and
+            'ends the routine
+            '
+            Exit Sub
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' Call ShowDialog.
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+
+            ' Get the file name.
+            Dim path As String = OpenFileDialog1.FileName
+
+            Dim SplitLine() As String
+            Dim TextLine As String = ""
+
+            Try
+
+                If System.IO.File.Exists(path) = True Then
+                    Using objReader As New System.IO.StreamReader(path, Encoding.ASCII)
+                        Do While objReader.Peek() <> -1
+                            TextLine = objReader.ReadLine()
+                            'SplitLine = Split(TextLine, ";")
+                            Me.DataGridView1.Rows.Add("", TextLine) ' SplitLine)
+                        Loop
+                    End Using
+                Else
+                    MsgBox("File Does Not Exist")
+                End If
+
+            Catch ex As Exception
+
+                ' Report an error.
+                Me.Text = "Error"
+
+            End Try
+
+        End If
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        MsgBox("Applied Systems 2023 - FS" & vbCrLf & "Version 1.0", vbInformation, "About")
+
+    End Sub
+End Class
